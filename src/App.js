efa6f37ca468736a3c645016fc919907ec69b23e
Javier Bórquez <hi@javier.xyz>
2016-03-23 21:21:33 -0600

    Update meta

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ var tinycolor = require('tinycolor2');
 var _ = require('lodash')
 var Dropzone = require('react-dropzone')
 
+var compressColor = require('./lib/compress-color.js');
+
 var {base64ImageToRGBArray} = require('base64-image-utils')
 
 function rgbToString(rgb) {
@@ -22,23 +24,6 @@ function rgbArrayToShadow(rgbArray, scale) {
 
     return `${color} ${pixel.x ? pixel.x * scale + 'px' : 0} ${pixel.y ? pixel.y * scale + 'px' : 0}`
   }).compact().value().join(',')
-}
-
-function compressColor (rgb) {
-  var hex = tinycolor(rgb).toHexString()
-
-  switch (hex) { // based on CSS3 supported color names http://www.w3.org/TR/css3-color/
-    case '#c0c0c0': return 'silver';
-    case '#808080': return 'gray';
-    case '#800000': return 'maroon';
-    case '#ff0000': return 'red';
-    case '#800080': return 'purple';
-    case '#008000': return 'green';
-    case '#808000': return 'olive';
-    case '#000080': return 'navy';
-    case '#008080': return 'teal';
-  }
-  return hex[1] === hex[2] && hex[3] === hex[4] && hex[5] === hex[6] ? "#" + hex[1] + hex[3] + hex[5] : hex;
 }
 
 export const App = React.createClass({
---
+++ src/lib/compress-color.js
@@ -0,0 +1,20 @@
+var tinycolor = require('tinycolor2');
+
+function compressColor (rgb) {
+  var hex = tinycolor(rgb).toHexString()
+
+  switch (hex) { // based on CSS3 supported color names http://www.w3.org/TR/css3-color/
+    case '#c0c0c0': return 'silver'
+    case '#808080': return 'gray'
+    case '#800000': return 'maroon'
+    case '#ff0000': return 'red'
+    case '#800080': return 'purple'
+    case '#008000': return 'green'
+    case '#808000': return 'olive'
+    case '#000080': return 'navy'
+    case '#008080': return 'teal'
+  }
+  return hex[1] === hex[2] && hex[3] === hex[4] && hex[5] === hex[6] ? "#" + hex[1] + hex[3] + hex[5] : hex;
+}
+
+module.exports = compressColor
