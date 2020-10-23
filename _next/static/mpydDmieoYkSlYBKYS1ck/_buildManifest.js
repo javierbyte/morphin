070b98fb6dff7f3029945ee9ade19c8d5e19d1c7
jb <hi@javier.xyz>
2020-10-23 23:20:19 +0200

    Updates

---
+++ _next/static/mpydDmieoYkSlYBKYS1ck/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002F29107295.f630771ff7b18d34618b.js","static\u002Fchunks\u002Fpages\u002Findex-019104f3ac87c327574b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-bf4eeb12d1016265a99a.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
