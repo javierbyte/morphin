574794079aed8b6a1f8ae0db3a94dec29b329773
jb <hi@javier.xyz>
2020-10-23 23:42:07 +0200

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "350a04ab6111067b5ce6";
+/******/ 	var hotCurrentHash = "08b8c3447f425c52b7cf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
