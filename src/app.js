37f184209fc1efe1a9d2f964ece021c6a179f921
Javier <hi@javier.xyz>
2021-08-21 00:46:07 +0200

    Update dependencies

--- src/app.js
+++ src/app.js
@@ -136,7 +136,10 @@ const SORT_METHODS = {
 function rgbArrayToShadow(rgbArray, { sortMethod = "Brightness", scale = 1 }) {
   return _.chain(rgbArray)
     .filter((pixel) => {
-      return !(pixel.rgb.a < 1 || (pixel.rgb.r > 254 && pixel.rgb.g > 254 && pixel.rgb.b > 254));
+      return !(
+        pixel.rgb.a < 1 ||
+        (pixel.rgb.r > 254 && pixel.rgb.g > 254 && pixel.rgb.b > 254)
+      );
     })
     .sortBy(SORT_METHODS[sortMethod])
     .map((pixel) => {
@@ -176,8 +179,11 @@ function saveFrame(path, config, stateSet) {
 }
 
 function App() {
-  const [animationSpeedSrc, animationSpeedSet] = useState(Math.round(Math.pow(1000, 1 / LOG_SCALE_FACTOR)));
-  const animationSpeed = Math.round(Math.pow(animationSpeedSrc, LOG_SCALE_FACTOR)) + 11 + 16;
+  const [animationSpeedSrc, animationSpeedSet] = useState(
+    Math.round(Math.pow(1000, 1 / LOG_SCALE_FACTOR))
+  );
+  const animationSpeed =
+    Math.round(Math.pow(animationSpeedSrc, LOG_SCALE_FACTOR)) + 11 + 16;
   const [animationTransition, animationTransitionSet] = useState(25);
   const [alternateAnimation, alternateAnimationSet] = useState(true);
 
@@ -199,8 +205,16 @@ function App() {
   useEffect(() => {
     if (!currentSprite) return;
 
-    saveFrame(`${document.location.href}/sprites/${defaultSprites[currentSprite].sprites[0]}`, { scale }, spriteASet);
-    saveFrame(`${document.location.href}/sprites/${defaultSprites[currentSprite].sprites[1]}`, { scale }, spriteBSet);
+    saveFrame(
+      `${document.location.href}/sprites/${defaultSprites[currentSprite].sprites[0]}`,
+      { scale },
+      spriteASet
+    );
+    saveFrame(
+      `${document.location.href}/sprites/${defaultSprites[currentSprite].sprites[1]}`,
+      { scale },
+      spriteBSet
+    );
 
     // const { scale = 3, transition = 25, alternate = false } = defaultSprites[currentSprite];
 
@@ -218,7 +232,9 @@ function App() {
     scaleSet(defaultSprites[currentSprite].scale || 3);
     animationTransitionSet(defaultSprites[currentSprite].transition || 25);
     alternateAnimationSet(
-      defaultSprites[currentSprite].alternate === undefined ? true : defaultSprites[currentSprite].alternate
+      defaultSprites[currentSprite].alternate === undefined
+        ? true
+        : defaultSprites[currentSprite].alternate
     );
     sortMethodSet(defaultSprites[currentSprite].sortMethod || "Vertical");
   }, [currentSprite]);
@@ -249,7 +265,9 @@ function App() {
 .pixel {
   height: ${scale}px;
   width: ${scale}px;
-  margin: ${-scale}px ${totalHeight + scale * 2}px ${totalWidth + scale * 2}px ${-scale}px;
+  margin: ${-scale}px ${totalHeight + scale * 2}px ${
+    totalWidth + scale * 2
+  }px ${-scale}px;
   animation: morphin ${animationSpeed}ms infinite${
     alternateAnimation ? " alternate " : " "
   }cubic-bezier(0.45, 0, 0.55, 1);
@@ -284,236 +302,248 @@ function App() {
   return (
     <Container>
       <JBX accent={"#ab99cf"} />
-      <Box padding={2}>
-        <Space h={1} />
-        <HeaderH1
-          style={{
-            fontWeight: 900,
-            display: "inline-block",
-            width: "auto",
-            padding: "6px",
-            backgroundColor: "var(--accent-color)",
-          }}>
-          morphin
-        </HeaderH1>
-        <Space h={1} />
-        <Text>
-          Tool that creates animated CSS transitions. Add sprites and get the code ready to paste in your site.
-        </Text>
-        <Space h={2} />
-        <Tabs>
-          <Inline>
-            <Tab info>
-              <Text>Examples: </Text>
-            </Tab>
-            {Object.keys(defaultSprites).map((spriteKey) => (
-              <Tab
-                active={currentSprite === spriteKey}
-                key={spriteKey}
-                onClick={() => {
-                  currentSpriteSet(spriteKey);
-                }}>
-                <Text>{spriteKey}</Text>
-              </Tab>
-            ))}
-          </Inline>
-        </Tabs>
-        <Space h={1} />
+      <Space h={1} />
+      <HeaderH1
+        style={{
+          fontWeight: 900,
+          display: "inline-block",
+          width: "auto",
+          padding: "6px",
+          backgroundColor: "var(--accent-color)",
+        }}>
+        morphin
+      </HeaderH1>
+      <Space h={1} />
+      <Text>
+        Tool that creates animated CSS transitions. Add sprites and get the code
+        ready to paste in your site.
+      </Text>
+      <Space h={2} />
+      <Tabs>
         <Inline>
-          <Dropzone
-            onDrop={(evt) => onFileSelected(evt, spriteASet)}
-            onDragOver={eventIgnore}
-            onDragEnter={eventIgnore}>
-            <div
-              style={{
-                display: "flex",
-                height: 48,
-                width: 48,
-                alignItems: "center",
-                alignContent: "center",
-                justifyContent: "center",
-              }}>
-              <Sprite width={spriteA.width} height={spriteA.height} aria-label="Sprite A" src={spriteA.src} />
-            </div>
-
-            <Text>Sprite 1</Text>
-            <input
-              type="file"
-              onChange={(evt) => onFileSelected(evt, spriteASet, true)}
-              accept="image/*"
-              aria-label="Drop an image here, or click to select"
-            />
-          </Dropzone>
-
-          <Space h={1} w={1} />
-
-          <Dropzone
-            onDrop={(evt) => onFileSelected(evt, spriteBSet)}
-            onDragOver={eventIgnore}
-            onDragEnter={eventIgnore}>
-            <div
-              style={{
-                display: "flex",
-                height: 48,
-                width: 48,
-                alignItems: "center",
-                alignContent: "center",
-                justifyContent: "center",
-              }}>
-              <Sprite width={spriteB.width} height={spriteB.height} aria-label="Sprite B" src={spriteB.src} />
-            </div>
-
-            <Text>Sprite 2</Text>
-            <input
-              type="file"
-              onChange={(evt) => onFileSelected(evt, spriteBSet)}
-              accept="image/*"
-              aria-label="Drop an image here, or click to select"
-            />
-          </Dropzone>
-        </Inline>
-        <Space h={2} />
-
-        <HeaderH2>Preview</HeaderH2>
-        <Space h={1} />
-        <style>{css}</style>
-        <div key={css} className="pixel" />
-        <Space h={2} />
-        <Tabs>
-          <Inline>
-            <Tab info>
-              <Text>Pixel matching method:</Text>
-            </Tab>
-            {Object.keys(SORT_METHODS).map((sortMethodKey) => (
-              <Tab
-                active={sortMethod === sortMethodKey}
-                key={sortMethodKey}
-                onClick={() => {
-                  sortMethodSet(sortMethodKey);
-                }}>
-                <Text>{sortMethodKey}</Text>
-              </Tab>
-            ))}
-          </Inline>
-        </Tabs>
-        <Space h={1} />
-        <Tabs>
-          <Inline>
-            <Tab info>
-              <Text>Alternate:</Text>
-            </Tab>
+          <Tab info>
+            <Text>Examples: </Text>
+          </Tab>
+          {Object.keys(defaultSprites).map((spriteKey) => (
             <Tab
-              active={alternateAnimation === true}
+              active={currentSprite === spriteKey}
+              key={spriteKey}
               onClick={() => {
-                alternateAnimationSet(true);
+                currentSpriteSet(spriteKey);
               }}>
-              <Text>Yes</Text>
+              <Text>{spriteKey}</Text>
             </Tab>
-            <Tab
-              active={alternateAnimation === false}
-              onClick={() => {
-                alternateAnimationSet(false);
-              }}>
-              <Text>No</Text>
-            </Tab>
-          </Inline>
-        </Tabs>
-        <Space h={1} />
-        <Inline h={-1}>
-          <Box flex={1} padding={[0.5, 1]} minWidth={"240px"}>
-            <Text>
-              Scale <span style={{ color: "#666" }}>{scale}</span>
-            </Text>
-            <Space h={1} />
-            <Range
-              aria-label="Scale"
-              type="range"
-              value={scale}
-              onChange={(e) => scaleSet(Number(e.target.value))}
-              min="1"
-              max="25"
-              step="0.5"
-            />
-          </Box>
-
-          <Box flex={1} padding={[0.5, 1]} minWidth={"240px"}>
-            <Text>
-              Transition <span style={{ color: "#666" }}>{animationTransition}%</span>
-            </Text>
-            <Space h={1} />
-            <Range
-              aria-label="Transition Speed"
-              type="range"
-              value={animationTransition}
-              onChange={(e) => animationTransitionSet(e.target.value)}
-              min="1"
-              max="100"
-            />
-          </Box>
-
-          <Box flex={1} padding={[0.5, 1]} minWidth={"240px"}>
-            <Text>
-              Speed <span style={{ color: "#666" }}>{animationSpeed}ms</span>
-            </Text>
-            <Space h={1} />
-            <Range
-              aria-label="Animation Speed"
-              type="range"
-              value={animationSpeedSrc}
-              onChange={(e) => animationSpeedSet(e.target.value)}
-              step="0.5"
-              min="2"
-              max={Math.ceil(Math.pow(16384 / 2, 1 / LOG_SCALE_FACTOR))}
-            />
-          </Box>
+          ))}
         </Inline>
-        <Space h={2} />
-        <HeaderH3>Code</HeaderH3>
-        <Space h={1} />
-        <code>
-          <Code aria-label="Generated code" onChange={() => {}} className="code" value={css} />
-        </code>
-        <Space h={1} />
-        {navigator && navigator.clipboard && navigator.clipboard.writeText && (
-          <Button
-            onClick={() => {
-              navigator.clipboard.writeText(css);
+      </Tabs>
+      <Space h={1} />
+      <Inline>
+        <Dropzone
+          onDrop={(evt) => onFileSelected(evt, spriteASet)}
+          onDragOver={eventIgnore}
+          onDragEnter={eventIgnore}>
+          <div
+            style={{
+              display: "flex",
+              height: 48,
+              width: 48,
+              alignItems: "center",
+              alignContent: "center",
+              justifyContent: "center",
             }}>
-            Copy Code
-          </Button>
-        )}
-        <Space h={2} />
-        <Text>
-          For a simpler image to box-shadow conversion, see <A href="https://javier.xyz/img2css/">img2css</A>.
-        </Text>
-        <Space h={2} />
-        <Text>More unrelated experiments</Text>
-        <Space h={0.5} />
+            <Sprite
+              width={spriteA.width}
+              height={spriteA.height}
+              aria-label="Sprite A"
+              src={spriteA.src}
+            />
+          </div>
+
+          <Text>Sprite 1</Text>
+          <input
+            type="file"
+            onChange={(evt) => onFileSelected(evt, spriteASet, true)}
+            accept="image/*"
+            aria-label="Drop an image here, or click to select"
+          />
+        </Dropzone>
+
+        <Space h={1} w={1} />
+
+        <Dropzone
+          onDrop={(evt) => onFileSelected(evt, spriteBSet)}
+          onDragOver={eventIgnore}
+          onDragEnter={eventIgnore}>
+          <div
+            style={{
+              display: "flex",
+              height: 48,
+              width: 48,
+              alignItems: "center",
+              alignContent: "center",
+              justifyContent: "center",
+            }}>
+            <Sprite
+              width={spriteB.width}
+              height={spriteB.height}
+              aria-label="Sprite B"
+              src={spriteB.src}
+            />
+          </div>
+
+          <Text>Sprite 2</Text>
+          <input
+            type="file"
+            onChange={(evt) => onFileSelected(evt, spriteBSet)}
+            accept="image/*"
+            aria-label="Drop an image here, or click to select"
+          />
+        </Dropzone>
+      </Inline>
+      <Space h={2} />
+
+      <HeaderH2>Preview</HeaderH2>
+      <Space h={1} />
+      <style>{css}</style>
+      <div key={css} className="pixel" />
+      <Space h={2} />
+      <Tabs>
+        <Inline>
+          <Tab info>
+            <Text>Pixel matching method:</Text>
+          </Tab>
+          {Object.keys(SORT_METHODS).map((sortMethodKey) => (
+            <Tab
+              active={sortMethod === sortMethodKey}
+              key={sortMethodKey}
+              onClick={() => {
+                sortMethodSet(sortMethodKey);
+              }}>
+              <Text>{sortMethodKey}</Text>
+            </Tab>
+          ))}
+        </Inline>
+      </Tabs>
+      <Space h={1} />
+      <Tabs>
+        <Inline>
+          <Tab info>
+            <Text>Alternate:</Text>
+          </Tab>
+          <Tab
+            active={alternateAnimation === true}
+            onClick={() => {
+              alternateAnimationSet(true);
+            }}>
+            <Text>Yes</Text>
+          </Tab>
+          <Tab
+            active={alternateAnimation === false}
+            onClick={() => {
+              alternateAnimationSet(false);
+            }}>
+            <Text>No</Text>
+          </Tab>
+        </Inline>
+      </Tabs>
+      <Space h={1} />
+      <Inline h={-1}>
+        <Box flex={1} padding={[0.5, 1]} minWidth={"240px"}>
+          <Text>
+            Scale <span style={{ color: "#666" }}>{scale}</span>
+          </Text>
+          <Space h={1} />
+          <Range
+            aria-label="Scale"
+            type="range"
+            value={scale}
+            onChange={(e) => scaleSet(Number(e.target.value))}
+            min="1"
+            max="25"
+            step="0.5"
+          />
+        </Box>
+
+        <Box flex={1} padding={[0.5, 1]} minWidth={"240px"}>
+          <Text>
+            Transition{" "}
+            <span style={{ color: "#666" }}>{animationTransition}%</span>
+          </Text>
+          <Space h={1} />
+          <Range
+            aria-label="Transition Speed"
+            type="range"
+            value={animationTransition}
+            onChange={(e) => animationTransitionSet(e.target.value)}
+            min="1"
+            max="100"
+          />
+        </Box>
+
+        <Box flex={1} padding={[0.5, 1]} minWidth={"240px"}>
+          <Text>
+            Speed <span style={{ color: "#666" }}>{animationSpeed}ms</span>
+          </Text>
+          <Space h={1} />
+          <Range
+            aria-label="Animation Speed"
+            type="range"
+            value={animationSpeedSrc}
+            onChange={(e) => animationSpeedSet(e.target.value)}
+            step="0.5"
+            min="2"
+            max={Math.ceil(Math.pow(16384 / 2, 1 / LOG_SCALE_FACTOR))}
+          />
+        </Box>
+      </Inline>
+      <Space h={2} />
+      <HeaderH3>Code</HeaderH3>
+      <Space h={1} />
+      <code>
+        <Code
+          aria-label="Generated code"
+          onChange={() => {}}
+          className="code"
+          value={css}
+        />
+      </code>
+      <Space h={1} />
+      {navigator && navigator.clipboard && navigator.clipboard.writeText && (
+        <Button
+          onClick={() => {
+            navigator.clipboard.writeText(css);
+          }}>
+          Copy Code
+        </Button>
+      )}
+      <Space h={2} />
+      <Text>
+        For a simpler image to box-shadow conversion, see{" "}
+        <A href="https://javier.xyz/img2css/">img2css</A>.
+      </Text>
+      <Space h={2} />
+      <Text>More unrelated experiments</Text>
+      <Space h={0.5} />
+      <Text>
         <Ul>
           <Li>
-            <Text>
-              {"Create more cohesive color schemes, "}
-              <A href="https://javier.xyz/cohesive-colors/">cohesive-colors</A>.
-            </Text>
+            {"Create more cohesive color schemes, "}
+            <A href="https://javier.xyz/cohesive-colors/">cohesive-colors</A>.
           </Li>
           <Li>
-            <Text>
-              Find the visual center of your images / logos,{" "}
-              <A href="https://javier.xyz/visual-center/">visual-center</A>.
-            </Text>
+            Find the visual center of your images / logos,{" "}
+            <A href="https://javier.xyz/visual-center/">visual-center</A>.
           </Li>
           <Li>
-            <Text>
-              JS AI Battle Game, <A href="https://clashjs.com/">clashjs</A>.
-            </Text>
+            JS AI Battle Game, <A href="https://clashjs.com/">clashjs</A>.
           </Li>
         </Ul>
-        <Space h={2} />
-        <Text>
-          Made by <A href="https://javier.xyz">javierbyte</A>.
-        </Text>
-        <Space h={3} />
-      </Box>
+      </Text>
+
+      <Space h={2} />
+      <Text>
+        Made by <A href="https://javier.xyz">javierbyte</A>.
+      </Text>
     </Container>
   );
 }
